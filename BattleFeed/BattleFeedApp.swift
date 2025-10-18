ffd8870391bcf58aad028c5258f90670a4f0d45c
JiteshDhumal63 <54475599+jitesh124201@users.noreply.github.com>
2025-10-18 12:32:26 +0530

    Initial commit - BattleFeed assignment

---
+++ BattleFeed/BattleFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleFeedApp.swift
+//  BattleFeed
+//
+//  Created by Jitesh Dhumal on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BattleFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
